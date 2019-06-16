3d46c4d62c293fa437e6d057cf2187f032f5fb80
jeong-yeeun <jeong-yeeun@192.168.168.129>
2019-06-17 00:22:03 +0900

    lab_test commit

--- src/main/java/SimpleChat/ChatServer.java
+++ src/main/java/SimpleChat/ChatServer.java
@@ -1,6 +1,8 @@
 //https://github.com/YeeunJ/SimpleChat/blob/master/ChatServer.java
+package SimpleChat;
 
 import java.net.*;
+import java.text.SimpleDateFormat;
 import java.io.*;
 import java.util.*;
 
@@ -13,11 +15,16 @@ public class ChatServer {
 			ServerSocket server = new ServerSocket(10001); // 클라이언트의 요청을 받기 위한 준비(10001번 포트)->다른 서버와 연결하기 위한 것,
 			System.out.println("Waiting connection...");
 			HashMap<String, PrintWriter> hm = new HashMap<String, PrintWriter>();//. Key와 value를 묶어 하나의 entry로 저장, 많은양의 데이터를 검색하는데 뛰어남
-			//스레드간 OutputStream을 공유하는 방법이 필요해서 해시맵 사용(id와 내용을 저장해서 
-			//ChatClient가 접속 요청을 한다.
+			ArrayList<String> p_word = new ArrayList<String>();
+			p_word.add("dog");
+			p_word.add("drive");
+			p_word.add("peach");
+			p_word.add("stupid");
+			p_word.add("monkey");
+			
 			while(true){
 				Socket sock = server.accept();//서버소켓으로부터 소켓 객체 가지고 오기(연결)
-				ChatThread chatthread = new ChatThread(sock, hm);
+				ChatThread chatthread = new ChatThread(sock, hm, p_word);
 				chatthread.start();
 				//새로운 쓰레드가 작업을 실행하는데 필요한 호출스택(공간)을 생성한 다음 run()을 호출해서 그 안(스택)에 run()이 저장되는 것
 				//id와 내용을 받아서 일정 형식으로 바꾼 후 다른 sock에 보내주는 등의 통신과 통신을 마무리하는 것은 chatthread에서 담당함
@@ -39,11 +46,12 @@ class ChatThread extends Thread{
 	private HashMap<String, PrintWriter> hm;
 	private boolean initFlag = false;
 	private PrintWriter pw_now;//계속 사용할 것이므로 현재 스레드의 printwriter를 먼저 만들어줌
-	
+	private ArrayList<String> p_word;
 	//constructor->새로운 sock가 생길때 pw, br을 만들어서 그 sock의 내용을 받고 hm에 저장해준다
-	public ChatThread(Socket sock, HashMap<String, PrintWriter> hm){
+	public ChatThread(Socket sock, HashMap<String, PrintWriter> hm, ArrayList<String> p_word){
 		this.sock = sock;
 		this.hm = hm;
+		this.p_word = p_word;
 		//sock, hm을 받아와서 초기화해주기
 		
 		try{
@@ -53,7 +61,7 @@ class ChatThread extends Thread{
 			this.pw_now = pw;
 			
 			broadcast(id + " entered.");//id entered 라는 내용을 sock에 보내기
-			System.out.println("[Server] User (" + id + ") entered.");//server의 콘솔 창에 들어왔다고 출력
+			System.out.println(print_time() + "[Server] User (" + id + ") entered.");//server의 콘솔 창에 들어왔다고 출력
 			synchronized(hm){//스레드 끼리 동기화
 				hm.put(this.id, pw);//key가 sock에서 준 id고 value는 그 id를 가진 sock로 보낼 pw 값
 			}
@@ -65,19 +73,25 @@ class ChatThread extends Thread{
 	
 	//전체적으로 실행해줌-> 각 명령에 따라서 적절하게 함수를 사용하여 출력해주고 sock가 나갈 경우 삭제하는 역할을 모두 총 명령을 내리는 역할
 	public void run(){
+		
 		try{
 			String line = null;
 			//키보드를 통해 입력되는 것에 따라서 종료하거나 귓속말을 하거나 전체로 출력함
 			while((line = br.readLine()) != null){
-				if(prohibit_word(line)) //만약 금지된 단어가 포함되어있으면 출력하지 않고 넘어감.
+				if(prohibit_word(line, p_word)) //만약 금지된 단어가 포함되어있으면 출력하지 않고 넘어감.
 					continue;
+				
 				if(line.equals("/quit"))
 					break;
 				if(line.indexOf("/userlist") == 0){ //userlist 보여줌
 					send_userlist();
 				}else if(line.indexOf("/to ") == 0){
 					sendmsg(line);//sendmsg를 사용해서 특정 sock에게 전달
-				}else 
+				}else if(line.equals("/spamlist")) {
+					print_pword(p_word);
+				}else if(line.indexOf("/spamlist") == 0) {
+					add_pword(p_word, line);
+				}else
 					broadcast(id + " : " + line);//broadcast 사용해서 모든 sock에게 전달
 			}
 		}catch(Exception ex){//예외사항이 생기면 에러를 출력하고
@@ -86,7 +100,7 @@ class ChatThread extends Thread{
 			synchronized(hm){
 				hm.remove(id);
 			}
-			broadcast(id + " exited.");//broadcast method를 통해 iㅇd exit이란 메세지를 sock로 보내줌
+			broadcast(print_time() + id + " exited.");//broadcast method를 통해 iㅇd exit이란 메세지를 sock로 보내줌
 			try{
 				if(sock != null)
 					sock.close();
@@ -104,7 +118,7 @@ class ChatThread extends Thread{
 			Object obj = hm.get(to);//hm에 저장된 pw를 불러와서 obj에 담기 -> 그 pw로만 정보를 전달하기 위해서!!
 			if(obj != null){
 				PrintWriter pw = (PrintWriter)obj;//받아온 pw로 인스턴스화 하고
-				pw.println(id + " whisphered. : " + msg2);//그 pw로만 전달 준비
+				pw.println(print_time()+id + " whisphered. : " + msg2);//그 pw로만 전달 준비
 				pw.flush();//전달 -> 그 sock에서만 메세지를 받아서 출력해줄 수 있음
 			} // if
 		}
@@ -120,7 +134,7 @@ class ChatThread extends Thread{
 				PrintWriter pw = (PrintWriter)iter.next();//다음으로 넘겨주며 hm에 저장되어있었던 pw 정보를 가지고 인스턴스화 해서 출력해줌
 				//hm에 저장되어있는 pw가 현재 스레드의 printwriter인 pw_now와 같지 않은 경우에만 msg를 보내줌
 				if(pw != pw_now) {
-					pw.println(msg);//pw가 sock로 msg를 보내줌
+					pw.println(print_time() + msg);//pw가 sock로 msg를 보내줌
 					pw.flush();//
 				}
 			}
@@ -133,11 +147,11 @@ class ChatThread extends Thread{
 		synchronized(hm){//hm을 동기화
 			//동기화된 hm을 가지고 id인 key값을 현재 스레드에 출력
 			//출력할 때 앞에서 만든 현재 스레드의 printwriter 사용
-			pw_now.println("<userlist Info>");
+			pw_now.println(print_time()+ "\n<userlist Info>");
 			pw_now.flush();
 			for(Object key : hm.keySet()) {
-					pw_now.println(key);
-					pw_now.flush();
+				pw_now.println(key);
+				pw_now.flush();
 			}
 			//끝났으면 hm의 size를 출력하면 현재 사용자 수를 출력한 것
 			pw_now.println("count : "+ hm.size());
@@ -146,17 +160,52 @@ class ChatThread extends Thread{
 	}
 	
 	//금지된 단어가 있는지 확인하는 method
-	public boolean prohibit_word(String msg) {
-		String []p_word = {"dog", "drive", "peach", "stupid", "monkey"};
+	public boolean prohibit_word(String msg, ArrayList<String> p_word) {
 		//금지된 단어가 부분에라도 포함되면 위에서 만든 현재 아이디의 printwriter에 금지되었다고 말하기
-		for(String s: p_word) {
-			if(msg.indexOf(s) != -1) {
-				pw_now.println("Contains banned words. You can not this message.");
-				pw_now.flush();
-				
-				return true;
+		synchronized(p_word){
+			for(String s: p_word) {
+				if(msg.indexOf(s) != -1) {
+					pw_now.println(print_time() + "Contains banned words. You can not this message.");
+					pw_now.flush();
+					
+					return true;
+				}
 			}
 		}
 		return false;
+	}
+	
+	public void print_pword(ArrayList<String> p_word) {
+		synchronized(hm){//hm을 동기화
+			synchronized(p_word){
+				//동기화된 hm을 가지고 id인 key값을 현재 스레드에 출력
+				//출력할 때 앞에서 만든 현재 스레드의 printwriter 사용
+				pw_now.println(print_time()+ "\n<spamlist Info>");
+				pw_now.flush();
+				for(String word : p_word) {
+					pw_now.println(word);
+					pw_now.flush();
+				}
+			}
+		}
+	}
+	
+	public ArrayList<String> add_pword(ArrayList<String> p_word, String msg) {
+		int start = msg.indexOf(" ") +1;
+		synchronized(p_word){
+			if(start != -1){
+				String word = msg.substring(start);
+				p_word.add(word);
+			}
+		}
+		return p_word;
+	}
+		
+	
+	public String print_time() {
+		Date time = new Date();
+	    SimpleDateFormat format1 = new SimpleDateFormat ("[HH:mm:ss] ");
+	    String time_format = format1.format(time);
+	    return time_format;
 	}
 }
